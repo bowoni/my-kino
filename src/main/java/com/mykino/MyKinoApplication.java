2f5d2c353d848655d2a1693764946aab619477c4
bowoni <dev.bowon@gmail.com>
2026-03-20 22:31:45 +0900

    배포시 오류 수정

--- src/main/java/com/mykino/MyKinoApplication.java
+++ src/main/java/com/mykino/MyKinoApplication.java
@@ -2,11 +2,18 @@ package com.mykino;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class MyKinoApplication {
+public class MyKinoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MyKinoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MyKinoApplication.class, args);
